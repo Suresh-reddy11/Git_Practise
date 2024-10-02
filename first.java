159bd429b09b48f48e4bf2ddad6860ed6e6362d2
Suresh-reddy11 <pappireddysuresh11@gmail.com>
2024-10-02 11:46:49 +0530

    adding my first java project

---
+++ first.java
@@ -0,0 +1,33 @@
+// class main{
+//     public static void main(String[] args){
+//         System.out.println("hello java");
+//         System.out.println("hello java");
+//         System.out.print("hello java");
+        
+
+//     }
+// }
+
+// class main{
+//     public static void main(String[] args){
+//         int count=1;
+//         System.out.println(count);
+        
+//     }
+// }
+
+// class main{
+//     public static void main(String[] args){
+//         boolean a=true;
+//         boolean b=false;
+//         System.out.println(a);
+//         System.out.println(b);
+        
+//     }
+//}
+
+
+///////OPERATORS//////////////////
+
+
+
