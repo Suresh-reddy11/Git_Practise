fdfa2344a6789ea0dd254482d9ed9ee5e416d824
Suresh-reddy11 <pappireddysuresh11@gmail.com>
2024-10-02 12:06:38 +0530

    uncommenting the code in first.java file

--- first.java
+++ first.java
@@ -1,30 +1,30 @@
-// class main{
-//     public static void main(String[] args){
-//         System.out.println("hello java");
-//         System.out.println("hello java");
-//         System.out.print("hello java");
+class main{
+    public static void main(String[] args){
+        System.out.println("hello java");
+        System.out.println("hello java");
+        System.out.print("hello java");
         
 
-//     }
-// }
+    }
+}
 
-// class main{
-//     public static void main(String[] args){
-//         int count=1;
-//         System.out.println(count);
+class main1{
+    public static void main(String[] args){
+        int count=1;
+        System.out.println(count);
         
-//     }
-// }
+    }
+}
 
-// class main{
-//     public static void main(String[] args){
-//         boolean a=true;
-//         boolean b=false;
-//         System.out.println(a);
-//         System.out.println(b);
+class main2{
+    public static void main(String[] args){
+        boolean a=true;
+        boolean b=false;
+        System.out.println(a);
+        System.out.println(b);
         
-//     }
-//}
+    }
+}
 
 
 ///////OPERATORS//////////////////
